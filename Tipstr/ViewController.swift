7cfd2de95f3cb2d335e1f972faa988a51a3fdd3a
Francis Phan <fphan@yahoo-inc.com>
2015-01-18 16:43:06 -0800

    Initial commit.

--- Tipstr/ViewController.swift
+++ Tipstr/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var splitControl: UISegmentedControl!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var splitLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        billField.text = "$0.00"
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        splitLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var billAmount = NSString(string: billField.text).doubleValue
+        
+        let tipPercentages = [0.15, 0.18, 0.20, 0.22, 0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        var split = total / ((Double)(splitControl.selectedSegmentIndex) + 1)
+        
+        tipLabel.text = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
+        splitLabel.text = String(format: "%.2f", split)
+    
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
 }
 
